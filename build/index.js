eaa42bc5c10f5038d77d6a6fbdf2206699fbf79d
ndxbxrme <lewis_the_cat@hotmail.com>
2017-06-02 09:33:11 +0100

    first build

--- build/index.js
+++ build/index.js
@@ -1,6 +1,67 @@
 (function() {
   'use strict';
-  module.exports = function(ndx) {};
+  var superagent;
+
+  superagent = require('superagent');
+
+  module.exports = function(ndx) {
+    var apiKey, baseUrl, callbacks, fillTemplate, safeCallback, url;
+    apiKey = process.env.EMAIL_API_KEY || ndx.settings.EMAIL_API_KEY;
+    baseUrl = process.env.EMAIL_BASE_URL || ndx.settings.EMAIL_BASE_URL;
+    fillTemplate = function(template, data) {
+      return template.replace(/\{\{(.+?)\}\}/g, function(all, match) {
+        var evalInContext;
+        evalInContext = function(str, context) {
+          return (new Function("with(this) {return " + str + "}")).call(context);
+        };
+        return evalInContext(match, data);
+      });
+    };
+    callbacks = {
+      send: [],
+      error: []
+    };
+    safeCallback = function(name, obj) {
+      var cb, i, len, ref, results;
+      ref = callbacks[name];
+      results = [];
+      for (i = 0, len = ref.length; i < len; i++) {
+        cb = ref[i];
+        results.push(cb(obj));
+      }
+      return results;
+    };
+    if (apiKey && baseUrl) {
+      url = baseUrl.replace('https://', "https://api:" + apiKey + "@");
+      return ndx.email = {
+        send: function(ctx, cb) {
+          var message;
+          if (process.env.EMAIL_OVERRIDE) {
+            ctx.to = process.env.EMAIL_OVERRIDE;
+          }
+          if (!process.env.EMAIL_DISABLE) {
+            message = {
+              from: ctx.from || from,
+              to: ctx.to,
+              subject: fillTemplate(ctx.subject, ctx),
+              html: jade.render(ctx.body, ctx)
+            };
+            console.log('sending', message);
+            return superagent.post(url).type('form').send(message).end(function(err, response) {
+              if (err) {
+                console.log(err);
+              }
+              if (response) {
+                return console.log(response.body);
+              }
+            });
+          } else {
+            return console.log('mail disabled');
+          }
+        }
+      };
+    }
+  };
 
 }).call(this);
 
--- src/index.coffee
+++ src/index.coffee
@@ -1,3 +1,43 @@
 'use strict'
 
+superagent = require 'superagent'
+
 module.exports = (ndx) ->
+  apiKey = process.env.EMAIL_API_KEY or ndx.settings.EMAIL_API_KEY
+  baseUrl = process.env.EMAIL_BASE_URL or ndx.settings.EMAIL_BASE_URL
+  fillTemplate = (template, data) ->
+    template.replace /\{\{(.+?)\}\}/g, (all, match) ->
+      evalInContext = (str, context) ->
+        (new Function("with(this) {return #{str}}"))
+        .call context
+      evalInContext match, data
+  callbacks = 
+    send: []
+    error: []
+  safeCallback = (name, obj) ->
+    for cb in callbacks[name]
+      cb obj
+  if apiKey and baseUrl
+    url = baseUrl.replace 'https://', "https://api:#{apiKey}@"
+    ndx.email =
+      send: (ctx, cb) ->
+        if process.env.EMAIL_OVERRIDE
+          ctx.to = process.env.EMAIL_OVERRIDE
+        if not process.env.EMAIL_DISABLE
+          message =
+            from: ctx.from or from
+            to: ctx.to
+            subject: fillTemplate ctx.subject, ctx
+            html: jade.render ctx.body, ctx
+          console.log 'sending', message
+          superagent.post url
+          .type 'form'
+          .send message
+          .end (err, response) ->
+            if err
+              console.log err
+            if response
+              console.log response.body
+        else
+          console.log 'mail disabled'
+  
